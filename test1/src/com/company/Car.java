53fa1d3fb52da76f225f806bab2664789ad95227
JPQueirozPerez <jpqueirozperez@gmail.com>
2022-04-04 11:23:46 +0200

    Commit #3: create Car class & Dog class

---
+++ test1/src/com/company/Car.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Car {
+    String make;
+    int year;
+
+    public Car(String make, int year){
+        this.make = make;
+        this.year = year;
+    }
+}
