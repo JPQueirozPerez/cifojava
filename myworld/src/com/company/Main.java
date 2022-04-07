8768f5964ef34bc59495f79981ccee6dbaf40ca8
JPQueirozPerez <jpqueirozperez@gmail.com>
2022-04-07 10:14:06 +0200

    Commit #7: create project MyWorld

--- myworld/src/com/company/Main.java
+++ myworld/src/com/company/Main.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    static  Scanner reader = new Scanner(System.in);
 
     public static void main(String[] args) {
 	// write your code here
         ArrayList<User> users = new ArrayList<>();
-        Scanner reader = new Scanner(System.in);
-
-        while(true){
+        
+            menuOptions();
 
             //print mini-menu with 4 options
 
@@ -25,6 +25,36 @@ public class Main {
 
             //call-operation to depositInputOutput
             //call-operation to deposit, to charge, to add some money to the card
+
+    }
+
+    public static void menu() {
+        System.out.println("MENU");
+        System.out.println(" ");
+        System.out.println("0. Quit");
+        System.out.println("1. Create user");
+        System.out.println("2. Change pin");
+        System.out.println("3. Transfer");
+        System.out.println("4. Deposit");
+        System.out.println(" ");
+        System.out.println("Choose an option:");
+    }
+
+    public static void menuOptions(){
+        while (true) {
+            boolean exit = false;
+            menu();
+            int value = reader.nextInt();
+
+            switch (value) {
+                case 0 -> exit = true;
+                case 1 -> createUserInputOutput();
+                case 2 -> changePinInputOutput();
+                case 3 -> transferInputOutput();
+                case 4 -> depositInputOutput();
+                default -> System.out.println("Invalid option\n");
+            }
+            if (exit) break;
         }
     }
 
