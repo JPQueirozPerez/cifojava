bc7045e7e6e25c834a79861ecbd207abe7f681b7
Juan Pablo Queiroz Pérez <51370813+JPQueirozPerez@users.noreply.github.com>
2022-04-07 10:07:49 +0200

    Commit #6: Delete myworld directory

--- myworld/src/com/company/Card.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Card {
-    int number;
-    double amount;
-    String type;
-    int pin;
-}
--- myworld/src/com/company/User.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class User {
-    String name;
-    String surname;
-    String age;
-}
