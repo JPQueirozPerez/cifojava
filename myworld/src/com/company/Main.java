4c5312fd874cbef4faf62abf9ec9c3593499d25c
JPQueirozPerez <jpqueirozperez@gmail.com>
2022-04-07 10:17:43 +0200

    Commit #7: create project MyWorld

--- myworld/src/com/company/Main.java
+++ myworld/src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        ArrayList<User> users = new ArrayList<>();
+        //ArrayList<User> users = new ArrayList<>();
 
             menuOptions();
 
